c8d2f0397389f70a0fae72252cfbb31a57709874
marina.imajou <potage480@yahoo.co.jp>
2021-09-23 16:47:48 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -18,8 +18,8 @@ import utils.DBUtil;
  */
 @WebServlet("/edit")
 public class EditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,9 +27,9 @@ public class EditServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
@@ -42,8 +42,11 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
+        // メッセージデータが存在しているときのみ
         // メッセージIDをセッションスコープに登録
+        if(m != null) {
         request.getSession().setAttribute("message_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
